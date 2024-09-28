13f4b0d3910988a94d762467e7d229a15ed41e48
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-28 18:49:06 -0300

    Add Solution section

---
+++ front-end-sprint-2-semestre/src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card({img, title, text}) {
+    return (
+        <div className="min-h-[33.75rem] max-w-96 bg-white rounded-3xl p-5">
+            <div className="flex flex-col justify-center items-center">
+                <img className="max-w-[6.25rem]" src={img} alt={"Image " + title}/>
+                <h2 className="text-3xl my-3.5 font-bold">{title}</h2>
+            </div>
+            <div>
+                <p className="text-[1.125rem] text-justify">{text}</p>
+            </div>
+        </div>
+    )
+}
--- front-end-sprint-2-semestre/src/pages/Home.jsx
+++ front-end-sprint-2-semestre/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header.jsx/Header";
-import { Carousel } from "@material-tailwind/react";
+import {Carousel} from "@material-tailwind/react";
+import Card from "../components/Card/Card.jsx";
 
 export default function Home() {
     return (
@@ -26,6 +27,15 @@ export default function Home() {
                                 alt="Imagem arquibancada 3"
                             />
                         </Carousel>
+                    </div>
+                </section>
+
+                <section className="bg-[url('section_background.png')] bg-no-repeat bg-center bg-cover px-5 lg:px-20">
+                    <h1 className="text-4xl font-bold text-center md:text-start text-white pt-14 mb-14">Solução</h1>
+                    <div className="flex flex-wrap gap-3 justify-center">
+                        <Card img="cellphone.png" title="Highlights" text="Vídeos de Highlights: em um formato estilo TikTok, nossa plataforma exibirá os melhores momentos de cada corrida. Os vídeos serão criados pelos usuários e pela conta oficial da Fórmula E. Nosso objetivo é atrair novos fãs mostrando momentos marcantes das corridas e aumentar o interesse pelo esporte. Além disso, queremos que os fãs existentes compartilhem e assistam a momentos únicos, fortalecendo o engajamento da comunidade."></Card>
+                        <Card img="live.png" title="Transmissões ao vivo" text='Transmissão ao vivo: através de pesquisas percebemos que a cobertura de canais de TV não é satisfatória e que ainda não existe um aplicativo similar a "F1TV" para a Fórmula E. Portanto, temos como solução a transmissão de todas as corridas em tempo real para os usuários da nossa plataforma de forma gratuita com anúncios. Dessa forma, garantimos uma maior acessibilidade ao público e aumentamos o número de telespectadores das corridas.'></Card>
+                        <Card img="joystick.png" title="Game" text="Jogo de construção de carros: queremos implementar um jogo para criar carros personalizados, atraindo fãs desse segmento. O objetivo é aumentar o engajamento, permitindo que os usuários postem suas criações. As equipes oficiais da Fórmula E poderão votar periodicamente para escolher o melhor carro, que será usado nas próximas corridas. Também incluiremos um quiz para testar os conhecimentos dos usuários sobre a Fórmula E."></Card>
                     </div>
                 </section>
             </main>
