da632917f23098ed46d0d2298a335d4575e64d1a
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-28 18:52:28 -0300

    Fix image height

--- front-end-sprint-2-semestre/src/components/Card/Card.jsx
+++ front-end-sprint-2-semestre/src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({img, title, text}) {
     return (
         <div className="min-h-[33.75rem] max-w-96 bg-white rounded-3xl p-5">
             <div className="flex flex-col justify-center items-center">
-                <img className="max-w-[6.25rem]" src={img} alt={"Image " + title}/>
+                <img className="w-[6.25rem] h-[6.25rem]" src={img} alt={"Image " + title}/>
                 <h2 className="text-3xl my-3.5 font-bold">{title}</h2>
             </div>
             <div>
