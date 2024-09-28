afe0672c2ee7bec77116d3fac93fbf00a210523b
cerbonxd <felipecerboncinix@gmail.com>
2024-09-27 21:59:57 -0300

    First cmmit with header build

---
+++ front-end-sprint-2-semestre/src/components/Header.jsx/Header.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export default function Header() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    return (
+        <header className="bg-[url('header_background.png')] bg-no-repeat bg-center bg-cover min-h-[58.75rem] max-w-[120rem] mx-auto">
+            <div className="flex md:justify-center justify-end">
+                <nav>
+                    {/*Mobile Menu*/}
+                    <section className="flex md:hidden">
+                        {/*Hamburguer Icon*/}
+                        <div className="space-y-2 pt-8 pr-8" onClick={() => setIsNavOpen((prev) => !prev)}>
+                            <span className="block h-0.5 w-8 bg-[#019DCB]"></span>
+                            <span className="block h-0.5 w-8 bg-[#019DCB]"></span>
+                            <span className="block h-0.5 w-8 bg-[#019DCB]"></span>
+                        </div>
+
+                        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                            {/*Cross Icon*/}
+                            <div className="absolute top-0 right-0 px-8 py-8" onClick={() => setIsNavOpen(false)}>
+                                <svg
+                                    className="h-8 w-8 text-gray-600"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                    <line x1="18" y1="6" x2="6" y2="18" />
+                                    <line x1="6" y1="6" x2="18" y2="18" />
+                                </svg>
+                            </div>
+
+                            {/*Menu Mobile*/}
+                            <ul className="flex flex-col items-center justify-between min-h-[15.625rem]">
+                                <li className="text-[#019DCB] font-bold"><a href="/about">Problema</a></li>
+                                <li className="text-[#019DCB] font-bold"><a href="/portfolio">Soluçâo</a></li>
+                                <li className="text-[#019DCB] font-bold"><a href="/contact">Comparação</a></li>
+                                <li className="text-[#019DCB] font-bold"><a href="/contact">Login</a></li>
+                            </ul>
+                        </div>
+                    </section>
+
+                    {/*Menu Desktop*/}
+                    <ul className="hidden gap-2 md:flex pt-7">
+                        <li><a className="text-[#019DCB] font-bold" href="/about">|</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/about">Problema</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/about">|</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/portfolio">Soluçâo</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/about">|</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/contact">Comparação</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/about">|</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/contact">Login</a></li>
+                        <li><a className="text-[#019DCB] font-bold" href="/about">|</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
---
+++ front-end-sprint-2-semestre/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header.jsx/Header";
+
+export default function() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
