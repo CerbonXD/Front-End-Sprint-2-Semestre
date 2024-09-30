9a679c85b37492ec408fd41c7e0a276adff0574c
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2024-09-30 16:34:59 -0300

    Use grid layout for solution section

--- front-end-sprint-2-semestre/src/pages/Home.jsx
+++ front-end-sprint-2-semestre/src/pages/Home.jsx
@@ -32,10 +32,27 @@ export default function Home() {
 
                 <section className="bg-[url('section_background.png')] bg-no-repeat bg-center bg-cover px-5 lg:px-20 pb-14">
                     <h1 className="text-4xl font-bold text-center md:text-start text-white pt-14 mb-14">Solução</h1>
-                    <div className="flex flex-wrap gap-3 justify-center">
-                        <Card img="cellphone.png" title="Highlights" bg="1" text="Vídeos de Highlights: em um formato estilo TikTok, nossa plataforma exibirá os melhores momentos de cada corrida. Os vídeos serão criados pelos usuários e pela conta oficial da Fórmula E. Nosso objetivo é atrair novos fãs mostrando momentos marcantes das corridas e aumentar o interesse pelo esporte. Além disso, queremos que os fãs existentes compartilhem e assistam a momentos únicos, fortalecendo o engajamento da comunidade."></Card>
-                        <Card img="live.png" title="Transmissões ao vivo" bg="1" text='Transmissão ao vivo: através de pesquisas percebemos que a cobertura de canais de TV não é satisfatória e que ainda não existe um aplicativo similar a "F1TV" para a Fórmula E. Portanto, temos como solução a transmissão de todas as corridas em tempo real para os usuários da nossa plataforma de forma gratuita com anúncios. Dessa forma, garantimos uma maior acessibilidade ao público e aumentamos o número de telespectadores das corridas.'></Card>
-                        <Card img="joystick.png" title="Game" bg="1" text="Jogo de construção de carros: queremos implementar um jogo para criar carros personalizados, atraindo fãs desse segmento. O objetivo é aumentar o engajamento, permitindo que os usuários postem suas criações. As equipes oficiais da Fórmula E poderão votar periodicamente para escolher o melhor carro, que será usado nas próximas corridas. Também incluiremos um quiz para testar os conhecimentos dos usuários sobre a Fórmula E."></Card>
+                    <div className="flex justify-center">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center max-w-[73.5rem]">
+                            <Card
+                                img="cellphone.png"
+                                title="Highlights"
+                                bg="1"
+                                text="Vídeos de Highlights: em um formato estilo TikTok, nossa plataforma exibirá os melhores momentos de cada corrida. Os vídeos serão criados pelos usuários e pela conta oficial da Fórmula E. Nosso objetivo é atrair novos fãs mostrando momentos marcantes das corridas e aumentar o interesse pelo esporte. Além disso, queremos que os fãs existentes compartilhem e assistam a momentos únicos, fortalecendo o engajamento da comunidade."
+                            />
+                            <Card
+                                img="live.png"
+                                title="Transmissões ao vivo"
+                                bg="1"
+                                text="Transmissão ao vivo: através de pesquisas percebemos que a cobertura de canais de TV não é satisfatória e que ainda não existe um aplicativo similar a 'F1TV' para a Fórmula E. Portanto, temos como solução a transmissão de todas as corridas em tempo real para os usuários da nossa plataforma de forma gratuita com anúncios. Dessa forma, garantimos uma maior acessibilidade ao público e aumentamos o número de telespectadores das corridas."
+                            />
+                            <Card
+                                img="joystick.png"
+                                title="Game"
+                                bg="1"
+                                text="Jogo de construção de carros: queremos implementar um jogo para criar carros personalizados, atraindo fãs desse segmento. O objetivo é aumentar o engajamento, permitindo que os usuários postem suas criações. As equipes oficiais da Fórmula E poderão votar periodicamente para escolher o melhor carro, que será usado nas próximas corridas. Também incluiremos um quiz para testar os conhecimentos dos usuários sobre a Fórmula E."
+                            />
+                        </div>
                     </div>
                 </section>
 
@@ -64,9 +81,9 @@ export default function Home() {
 
                 <footer className="bg-[url('section_background.png')] bg-no-repeat bg-center bg-cover px-5 lg:px-20 pb-5 pt-28 text-white">
                     <div className="flex flex-wrap justify-center gap-10 mb-12">
-                        <img className="max-w-16" src="facebook.png" alt="Facebook logo" />
-                        <img className="max-w-16" src="instagram.png" alt="Instagram logo" />
-                        <img className="max-w-16" src="twitter.png" alt="Twitter logo" />
+                        <img className="max-w-16" src="facebook.png" alt="Facebook logo"/>
+                        <img className="max-w-16" src="instagram.png" alt="Instagram logo"/>
+                        <img className="max-w-16" src="twitter.png" alt="Twitter logo"/>
                         <img className="max-w-16" src="gmail.png" alt="Gmail logo" />
                         <img className="max-w-16" src="youtube.png" alt="YouTube logo" />
                     </div>
